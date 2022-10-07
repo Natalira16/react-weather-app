e26749c0c2cc7da4348160820935d6222bd16617
Natalira16 <medvednat@gmail.com>
2022-10-07 21:20:08 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App content">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Kyiv" defaultCountry="UA" />
       </div>
       <footer className="footer">
         This project was coded by <a href="https://funny-mooncake-671b77.netlify.app/" target="_blank" rel="noreferrer" className="footer-link">Nataliia Medvedenko</a> and is <a href="https://github.com/Natalira16/react-weather-app" target="_blank" rel="noreferrer" className="footer-link">open-sourced on GitHub</a>.
--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return (
-        <div className="Weather">
-            <div className="header">
-                <p>
-                    <a href="https://www.google.com/search?q=weather+berlin" className="city" id="quick-search_berlin">Berlin</a>
-                    <a href="https://www.google.com/search?q=weather+paris" className="city" id="quick-search_paris">Paris</a>
-                    <a href="https://www.google.com/search?q=weather+new+york" className="city" id="quick-search_newyork">New York</a>
-                    <a href="https://www.google.com/search?q=weather+sydney" className="city" id="quick-search_sydney">Sydney</a>
-                </p>
-            </div>
-            <form className="search-group">
-                <input type="search" autoComplete="off" autoFocus="on" className="form-control" id="form-control" placeholder="Enter a city..." aria-label="Search"></input>
-                <button type="button" className="btn-search" id="btn-search">Search</button>
-                <button type="button" className="btn-current" id="btn-current">Current</button>
-            </form>
-            <div className="main-content">
-                <div className="card-right">
-                    <h1 className="title">Kyiv, UA</h1>
-                    <ul>
-                        <li>Wednesday 10:00</li>
-                        <li>Cloudy</li>
-                    </ul>
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+        console.log(response.data);
+
+        setWeatherData({
+            ready: true,
+            city: response.data.name,
+            country: response.data.sys.country,
+            temperature: response.data.main.temp,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+            date: "Wednesday 10:00",
+            description: response.data.weather[0].description,
+            feels_like: response.data.main.feels_like,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed
+        })
+    }
+
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                <div className="header">
+                    <p>
+                        <a href="https://www.google.com/search?q=weather+berlin" className="city" id="quick-search_berlin">Berlin</a>
+                        <a href="https://www.google.com/search?q=weather+paris" className="city" id="quick-search_paris">Paris</a>
+                        <a href="https://www.google.com/search?q=weather+new+york" className="city" id="quick-search_newyork">New York</a>
+                        <a href="https://www.google.com/search?q=weather+sydney" className="city" id="quick-search_sydney">Sydney</a>
+                    </p>
                 </div>
-                <div className="card-left">
-                    <div className="items">
-                        <div className="icon-item">
-                            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" className="icon" width="90px" alt="Cloudy"></img>
-                        </div>
-                        <div className="text-items">
-                            <div className="main-text">
-                                <h3 className="card-title show-temp" id="temperature">13</h3>
-                                <div className="tepm-links units">
-                                    <a href="https://www.google.com" className="unitC active" id="celsius-btn"><span><strong>°C</strong></span></a>
-                                    <span className="vertical-bar"> | </span>
-                                    <a href="https://www.google.com" className="unitF" id="fahrenheit-btn"><span><strong>°F</strong></span></a>
+                <form className="search-group">
+                    <input type="search" autoComplete="off" autoFocus="on" className="form-control" id="form-control" placeholder="Enter a city..." aria-label="Search"></input>
+                    <button type="button" className="btn-search" id="btn-search">Search</button>
+                    <button type="button" className="btn-current" id="btn-current">Current</button>
+                </form>
+                <div className="main-content">
+                    <div className="card-right">
+                        <h1 className="title">{weatherData.city}, {weatherData.country}</h1>
+                        <ul>
+                            <li>{weatherData.date}</li>
+                            <li className="text-capitalize">{weatherData.description}</li>
+                        </ul>
+                    </div>
+                    <div className="card-left">
+                        <div className="items">
+                            <div className="icon-item">
+                                <img src={weatherData.iconUrl} className="icon" width="90px" alt={weatherData.description}></img>
+                            </div>
+                            <div className="text-items">
+                                <div className="main-text">
+                                    <h3 className="card-title show-temp" id="temperature">{Math.round(weatherData.temperature)}</h3>
+                                    <div className="tepm-links units">
+                                        <a href="https://www.google.com" className="unitC active" id="celsius-btn"><span><strong>°C</strong></span></a>
+                                        <span className="vertical-bar"> | </span>
+                                        <a href="https://www.google.com" className="unitF" id="fahrenheit-btn"><span><strong>°F</strong></span></a>
+                                    </div>
+                                </div>
+                                <div className="desc-items">
+                                    <ul>
+                                        <li className="item">Feels like: <span id="feels_like">{Math.round(weatherData.feels_like)}</span>°C</li>
+                                        <li className="item">Humidity: <span id="humidity">{weatherData.humidity}</span>%</li>
+                                        <li className="item">Wind: <span id="wind">{Math.round(weatherData.wind)}</span> km/h</li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div className="desc-items">
-                                <ul>
-                                    <li className="item">Precipitation: <span id="precipitation">66</span>%</li>
-                                    <li className="item">Humidity: <span id="precipitation">92</span>%</li>
-                                    <li className="item">Wind: <span id="precipitation">10</span> km/h</li>
-                                </ul>
-                            </div>
+
+
                         </div>
-
-
                     </div>
                 </div>
             </div>
+        )
+    } else {
+        const apiKey = "c9470fa529ce6770f386cc31e17d6a25";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity},${props.defaultCountry
+            }&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
 
-
-        </div>
-    )
+        return ("Loading...");
+    }
 }
