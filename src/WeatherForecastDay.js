523898bd3f1191fbf080dc99b4e367b2318ef348
Natalira16 <medvednat@gmail.com>
2022-10-16 15:33:48 +0200

    Forecast refresh (celsius) done

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,17 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-    function maxTemp() {
-        let temperature = Math.round(props.data.temp.max);
-        return `${temperature}°`;
-    }
-
-    function minTemp() {
-        let temperature = Math.round(props.data.temp.min);
-        return `${temperature}°`;
-    }
-
     function day() {
         let date = new Date(props.data.dt * 1000);
         let day = date.getDay();
@@ -19,14 +9,53 @@ export default function WeatherForecastDay(props) {
         return days[day];
     }
 
-    return (
-        <div>
-            <div className="WeatherForecast-day mb-2">{day()}</div>
-            <WeatherIcon code={props.data.weather[0].icon} size={42} />
-            <div className="WeatherForecast-temps mt-2">
-                <span className="WeatherForecast-max-temp">{maxTemp()}</span>
-                <span className="WeatherForecast-min-temp">{minTemp()}</span>
+
+    function maxCTemp() {
+        let tempMax = Math.round(props.data.temp.max);
+        return `${tempMax}°`;
+    }
+
+    function minCTemp() {
+        let tempMin = Math.round(props.data.temp.min);
+        return `${tempMin}°`;
+    }
+
+
+    const [unit, setUnit] = useState("celsius")
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    if (unit === "celsius") {
+        return (
+            <div>
+                <div className="WeatherForecast-day mb-2">{day()}</div>
+                <WeatherIcon code={props.data.weather[0].icon} size={42} />
+                <div className="WeatherForecast-temps mt-2">
+                    <span className="WeatherForecast-max-temp">{maxCTemp()}</span>
+                    <span className="WeatherForecast-min-temp">{minCTemp()}</span>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        let maxFTemp = ((maxCTemp * 9 / 5) + 32)
+        let minFTemp = ((minCTemp * 9 / 5) + 32)
+
+        return (
+            <div>
+                <div className="WeatherForecast-day mb-2">{day()}</div>
+                <WeatherIcon code={props.data.weather[0].icon} size={42} />
+                <div className="WeatherForecast-temps mt-2">
+                    <span className="WeatherForecast-max-temp">{maxFTemp()}</span>
+                    <span className="WeatherForecast-min-temp">{minFTemp()}</span>
+                </div>
+            </div>
+        )
+    }
 }
